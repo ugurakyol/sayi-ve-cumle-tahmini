d07158e163da7cd01fd22c50c8c201115691117b
Uğur AKYOL <34269191+ugurakyol@users.noreply.github.com>
2021-03-28 20:13:31 +0300

    Add project files.

---
+++ YoyunGo/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace YoyunGo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(318, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toplam";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(439, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(12, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(282, 388);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ YoyunGo/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoyunGo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int toplam=0;
+
+        public int seviye=1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getir(seviye);
+        }
+
+        private void getir(int basamak)
+        {
+            int sayi = 0;
+            switch (basamak)
+            {
+
+                case 1:
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(seviye + ". seviyedesiniz.");
+                    for (int i = 1; i < 11; i++)
+                    {
+                        Random random = new Random();
+                        sayi = random.Next(10);
+                        toplam = toplam + sayi;                        
+                        listBox1.Items.Add(i + ". sayı =  " + sayi);
+                    }
+                    break;
+                case 2:
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(seviye + ". seviyedesiniz.");
+                    for (int i = 1; i < 11; i++)
+                    {
+                        Random random = new Random();
+                        sayi = random.Next(10,100);
+                        toplam = toplam + sayi;
+                        listBox1.Items.Add(i + ". sayı =  " + sayi);
+                    }
+                    break;
+                case 3:
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(seviye + ". seviyedesiniz.");
+                    for (int i = 1; i < 11; i++)
+                    {
+                        Random random = new Random();
+                        sayi = random.Next(100,1000);
+                        toplam = toplam + sayi;
+                        listBox1.Items.Add(i + ". sayı =  " + sayi);
+                    }
+                    break;
+                case 4:
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(seviye + ". seviyedesiniz.");
+                    for (int i = 1; i < 6; i++)
+                    {
+                        Random random = new Random();
+                        sayi = random.Next(1000,10000);
+                        toplam = toplam + sayi;
+                        listBox1.Items.Add(i + ". sayı =  " + sayi);
+                    }
+                    break;
+                case 5:
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(seviye + ". seviyedesiniz.");
+                    for (int i = 1; i < 6; i++)
+                    {
+                        Random random = new Random();
+                        sayi = random.Next(10000,100000);
+                        toplam = toplam + sayi;
+                        listBox1.Items.Add(i + ". sayı =  " + sayi);
+                    }
+                    break;
+            }            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            int tahmin;
+            try
+            {
+                tahmin = int.Parse(textBox1.Text);
+                if (toplam == tahmin)
+                {
+                    label2.Text = "Tebrikler doğru tahmin. "+seviye+". seviyeyi başarıyla geçtiniz.";
+                    label2.ForeColor = Color.DarkGreen;
+                    toplam = 0;
+                    seviye++;
+                    getir(seviye);
+                }
+                else if (toplam > tahmin)
+                {
+                    label2.Text = "toplam daha büyük";
+                    label2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label2.Text = "toplam daha küçük";
+                    label2.ForeColor = Color.Blue;
+                }
+                label2.Visible = true;
+            }
+            catch (Exception h)
+            {
+                MessageBox.Show("Lütfen sayı değeri giriniz.");
+            }
+            
+            
+
+        }
+
+
+    }
+}
